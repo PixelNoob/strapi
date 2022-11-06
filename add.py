3855d679f0b9c22c11022a37fabf4f04a3293462
J. Chitty <jesuschitty7@hotmail.com>
2022-11-06 12:45:14 +0100

    Create add.py
    add new row to db

---
+++ add.py
@@ -0,0 +1,13 @@
+import requests as r
+import json
+
+headers = {'content-type': 'application/json', 'Authorization':'{}'.format(os.environ.get('strapi_token'))}
+
+data = {
+            "String": 'brasil campeone de futebol'
+    }
+
+data = json.dumps(data)
+
+res = r.post('http://208.85.21.227:1337/Tests', data=data, headers=headers).json()
+print(res)
